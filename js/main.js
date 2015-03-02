59eedc1c91280780b82639c9df9144ce4ac5d9ce
cmacharia <cmacharia1@gmail.com>
2015-03-02 01:34:38 -0500

    Merge branch 'master' into gh-pages

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// init Isotope
+var $container = $('.showcase-gallery').isotope('layout');
+
+// filter items on button click
+$('.showcase-filter').on( 'click', 'a', function(e) {
+    e.preventDefault();
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+  
+  $('.showcase-filter li').removeClass('active');
+  $(this).closest('li').addClass('active')
+});

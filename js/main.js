8ed73f8bf25af1f2bc211da7128ad05e5d4b26d1
Chris Macharia <cmacharia1@gmail.com>
2017-11-13 07:35:45 -0600

    Clean up of code with comments in css. Adding styles to replace bootstrap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-// init Isotope
-var $container = $('.showcase-gallery').isotope('layout');
+$(function() {
+        $(".intro h1").hide(0).delay(500).fadeIn(1000)
 
-// filter items on button click
-$('.showcase-filter').on( 'click', 'a', function(e) {
-    e.preventDefault();
-  var filterValue = $(this).attr('data-filter');
-  $container.isotope({ filter: filterValue });
+});
+
+
+// // init Isotope
+// var $container = $('.showcase-gallery').isotope('layout');
+
+
+// // filter items on button click
+// $('.showcase-filter').on( 'click', 'a', function(e) {
+//     e.preventDefault();
+//   var filterValue = $(this).attr('data-filter');
+//   $container.isotope({ filter: filterValue });
   
-  $('.showcase-filter li').removeClass('active');
-  $(this).closest('li').addClass('active')
-});
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
-});
+//   $('.showcase-filter li').removeClass('active');
+//   $(this).closest('li').addClass('active')
+// });
+// $(".nav a").on("click", function(){
+//    $(".nav").find(".active").removeClass("active");
+//    $(this).parent().addClass("active");
+// });
+
